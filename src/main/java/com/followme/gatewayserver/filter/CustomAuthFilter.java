d82bcc30370bf91fc917dd2a5473ac08f0a726f8
qldo <idzhdl@gmail.com>
2026-03-30 04:20:08 +0900

    fix: spotless 적용

--- src/main/java/com/followme/gatewayserver/filter/CustomAuthFilter.java
+++ src/main/java/com/followme/gatewayserver/filter/CustomAuthFilter.java
@@ -1,39 +1,36 @@
 package com.followme.gatewayserver.filter;
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Slf4j
 @Component
 public class CustomAuthFilter extends AbstractGatewayFilterFactory<CustomAuthFilter.Config> {
 
   // 인증을 면제할 공개(Public) API 경로 목록 정의 (화이트리스트)
-  private static final List<String> EXCLUDE_PATHS = List.of(
-      "/api/v1/users/register"
-  );
+  private static final List<String> EXCLUDE_PATHS = List.of("/api/v1/users/register");
 
   public CustomAuthFilter() {
     super(Config.class);
   }
 
-  public static class Config {
-  }
+  public static class Config {}
 
   @Override
   public GatewayFilter apply(Config config) {
     return (exchange, chain) -> {
-
       String path = exchange.getRequest().getURI().getPath();
       log.info("[CustomAuth Filter] KeyCloak authentication filter started. Path: {}", path);
 
       boolean isExcluded = EXCLUDE_PATHS.stream().anyMatch(path::startsWith);
       if (isExcluded) {
-        log.info("[CustomAuth Filter] Excluded path detected: {}. Proceeding without authentication.", path);
+        log.info(
+            "[CustomAuth Filter] Excluded path detected: {}. Proceeding without authentication.",
+            path);
         return chain.filter(exchange);
       }
 
