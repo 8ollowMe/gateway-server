23afd9754cf734044a0b1b8c5ffb928869e2de1d
qldo <idzhdl@gmail.com>
2026-04-02 15:48:33 +0900

    feat: WebClient 기반 API Gateway 실시간 인증 로직 구현

---
+++ src/main/java/com/followme/gatewayserver/config/WebClientConfig.java
@@ -0,0 +1,16 @@
+package com.followme.gatewayserver.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    @LoadBalanced // Eureka 네임서버를 통해 IP를 동적으로 찾아주는 핵심 어노테이션
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
---
+++ src/main/java/com/followme/gatewayserver/response/UserResponse.java
@@ -0,0 +1,7 @@
+package com.followme.gatewayserver.response;
+
+public record UserResponse(
+        boolean success,
+        User data,
+        String error
+) {}
